0f2fa97d28432073f17a731caf623d744558f9d6
Cyril Gravelier <magiccyril@gmail.com>
2013-03-06 15:11:34 +0100

    Remove unused aliases

--- .aliases.sh
+++ .aliases.sh
@@ -17,15 +17,3 @@ alias reload='source ~/.bash_profile'
 
 # Keep from clobbering directories
 alias mkdir='mkdir -p'
-
-# Who doesn't love tree's colored output?
-alias tree='tree -C'
-
-# Aliases to interact with GNU screen
-alias ss='screen -S'
-alias sr='screen -r'
-alias sls='screen -ls'
-# Don't forget about sk function to kill screens.
-
-# Apache
-alias a2='sudo apachectl'
